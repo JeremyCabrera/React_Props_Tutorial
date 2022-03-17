3e9706c1a286ceef898a86de69244611325cf768
Jeremy Cabrera <jeremy.cabrera@outlook.com>
2022-03-17 19:41:19 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        tel="918 372 574"
+        email="gmail@chucknorris.com"
+      />
+    </div>
+  );
+}
+
+export default App;
